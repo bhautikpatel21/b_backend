63b374876f3fb1b8ddb7017cf027838413d80ad1
bhautikpatel21 <158008833+bhautikpatel21@users.noreply.github.com>
2024-02-19 10:15:59 +0530

    19-02-2024

--- nodejs/fs.js
+++ nodejs/fs.js
@@ -1,21 +1,80 @@
 const fs = require("fs");
 
-// synchronously and Asynchronously mathod 
+// //const {add,product} = require("./basic.js");
 
-// Asynchronous Mathod 
+// const {product} = require('./basic.js');
 
-fs.open('abc.txt', (err,data)=>{
-    if(err){
-        console.log('Failed to open file');
-    }
-    else
-    {
-    console.log('File open sucesasafully');
-    }  
-})
+// console.log(add(5,3));
 
-// synronous Method 
+// synchronously and Asynchronously method 
+
+// Asynchronous Method 
+
+// fs.open('abc.txt', (err,data)=>{
+//     if(err){
+//         console.log('Failed to open file');
+//     }
+//     else
+    
+//     console.log('File open Successfully');
+    
+// })
+
+// Synchronously Method 
 
 //  fs.openSync('abc.txt');
 
-//  console.log('File open successfuly');
+//  console.log('File open Successfully');
+
+// Read Method
+
+// let t1 = performance.now();
+// console.log('Product is :',product(5,3));
+// fs.readFile('abc.txt','utf-8',(err,data)=>{
+//     if(err)
+//     console.log('Failed to read File');
+     
+//     else
+//     console.log(data);
+// });
+
+// let data = fs.readFileSync('abc.txt','utf-8');
+
+// console.log(data);
+// console.log('Addition is : ',add(15,13));
+// let t2 = performance.now();
+// console.log('Time difference is : ',t2-t1);
+
+// Write Method 
+
+// let txt = `#include <stdio.h>
+
+// int main ()
+// {
+//     // printf (" Good morning ");
+//     printf ("Learning NodeJS);
+// }`;
+
+// fs.writeFile("basic.c",txt,(err,data)=>}{
+//     if (err) console.log("Filed to write File");
+//     else console.log("File Written successfully")
+// });
+// fs.writeFileSync("hello.c",txt);
+
+// console.log("File Written successfully");
+
+// Append Method 
+
+// let txt = `// Good morning`;
+
+// fs.appendFile("basic.c",txt,(err,data)=>{
+//     if (err) console.log("Filed to write File");
+//     else console.log("File Append Successfully");
+// });
+
+// close 
+
+// fs.close(2,(err) => {
+//     if (err) console.log("Failed to close File");
+//     else console.log("File Closed Successfully");
+// })
--- nodejs/path.js
+++ nodejs/path.js
@@ -30,4 +30,4 @@ const path = require('path');
 
 // console.log(path.basename(__filename))
 
-console.log(path.basename(__dirname))
+// console.log(path.basename(__dirname))
